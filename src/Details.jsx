b8c894cdf4874d9722567f51e8f86a5d9b3e86a1
Murat Altun <mraltun@gmail.com>
2023-01-15 13:32:17 +0300

    Added header to App

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,5 +1,8 @@
+import { useParams } from "react-router-dom";
+
 const Details = () => {
-  return <div>Details</div>;
+  const { id } = useParams();
+  return <h2>{id}</h2>;
 };
 
 export default Details;
