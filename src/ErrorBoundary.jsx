3d87f3159770151d25372bcb6db0bfc92f7bce35
Murat Altun <mraltun@gmail.com>
2023-01-19 16:22:31 +0300

    Finished ErrorBoundary

--- src/ErrorBoundary.jsx
+++ src/ErrorBoundary.jsx
@@ -1,15 +1,21 @@
-// mostly code from reactjs.org/docs/error-boundaries.html
+// We're going to use a feature called componentDidCatch to handle this. This is something you can't do with hooks so if you needed this sort of functionality you'd have to use a class component.
+// A component can only catch errors in its children, so it cannot catch its own errors. This will be wrapper for Details to catch it's errors
 import { Component } from "react";
 import { Link } from "react-router-dom";
 
 class ErrorBoundary extends Component {
   state = { hasError: false };
+
+  // It's static because React has to call directly on the class, not new instance of the class "ErrorBoundary.getDerivedStateFromError(error)"
   static getDerivedStateFromError() {
     return { hasError: true };
   }
+
+  //
   componentDidCatch(error, info) {
     console.error("ErrorBoundary caught an error", error, info);
   }
+
   render() {
     if (this.state.hasError) {
       return (
@@ -20,6 +26,7 @@ class ErrorBoundary extends Component {
       );
     }
 
+    // If there is no error just show Details seamlessly
     return this.props.children;
   }
 }
