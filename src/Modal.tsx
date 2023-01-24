98eb00bb85d060de4ce19b4aea63a42676580c29
Murat Altun <mraltun@gmail.com>
2023-01-24 14:59:46 +0300

    Model refactor to tsx

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,20 +1,28 @@
 // Portal. This will mount a div and mount inside of the portal whenever the Modal is rendered and then remove itself whenever it's unrendered.
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, MutableRefObject, ReactElement } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = ({ children }) => {
+// Children will be the things React renders like jsx
+const Modal = ({ children }: { children: ReactElement }) => {
   // We use Ref to have same exact "div" in every re-render
-  const elRef = useRef(null);
+  const elRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
   if (!elRef.current) {
     elRef.current = document.createElement("div");
   }
 
   useEffect(() => {
     const modalRoot = document.getElementById("modal");
+    if (!modalRoot || !elRef.current) {
+      return;
+    }
     modalRoot.appendChild(elRef.current);
 
     // componentWillUnmount
-    return () => modalRoot.removeChild(elRef.current);
+    return () => {
+      if (elRef.current) {
+        modalRoot.removeChild(elRef.current);
+      }
+    };
   }, []);
 
   return createPortal(<div>{children}</div>, elRef.current);
