55b783e5e97213cc398e7fd7d9481936e87ba53e
Murat Altun <mraltun@gmail.com>
2023-01-24 16:26:50 +0300

    AdoptedPetContext refactor to tsx

--- src/AdoptedPetContext.ts
+++ src/AdoptedPetContext.ts
@@ -2,7 +2,20 @@
 // A Consumer is how you consume from the above provider. A Consumer accepts a function as a child and gives it the context which you can use. We won't be using the Consumer directly: a function called useContext will do that for us.
 // The object provided to context is the default state it uses when it can find no Provider above it, useful if there's a chance no provider will be there and for testing. It's also useful for TypeScript because TypeScript will enforce these types
 import { createContext } from "react";
+import { Pet } from "./APIResponsesTypes";
 
-const AdoptedPetContext = createContext();
+const AdoptedPetContext = createContext<[Pet, (adoptedPet: Pet) => void]>([
+  {
+    id: 1337,
+    name: "Fido",
+    animal: "dog",
+    description: "Lorem ipsum",
+    breed: "Beagle",
+    images: [],
+    city: "Seattle",
+    state: "WA",
+  },
+  () => {},
+]);
 
 export default AdoptedPetContext;
