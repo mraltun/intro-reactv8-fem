9abde91c635ef7bbadc5f635676427e28c0862ab
Murat Altun <mraltun@gmail.com>
2023-01-27 16:32:30 +0300

    Added adoptedPetSlice

---
+++ src/adoptedPetSlice.ts
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// Slice is what we're calling a bundle of reducers, state, and action creators.
+export const adoptedPetSlice = createSlice({
+  name: "adoptedPet",
+  initialState: {
+    value: null,
+  },
+  //  You give it any reducers we need in our case, we just want a simple action that sets whatever the payload is to be what's stored. This common. Sometimes you may want to do some processing or math or something like that.
+  reducers: {
+    adopt: (state, action) => {
+      state.value = action.payload;
+    },
+  },
+});
+
+// RTK takes the liberty of making action creators and the actual reducers for us
+export const { adopt } = adoptedPetSlice.actions;
+
+export default adoptedPetSlice.reducer;
