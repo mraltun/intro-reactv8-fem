7f22d322faea1879b821e49f859ab7555d7863a4
Murat Altun <mraltun@gmail.com>
2023-01-30 17:37:03 +0300

    Added Pet test file

--- src/Pet.tsx
+++ src/Pet.tsx
@@ -14,14 +14,14 @@ const Pet = (props: IProps) => {
   const { name, animal, breed, images, location, id } = props;
 
   let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
-  if (images.length) {
+  if (images && images.length) {
     hero = images[0];
   }
 
   return (
     <Link to={`/details/${id}`} className="relative block">
       <div>
-        <img src={hero} alt={name} />
+        <img data-testid="thumbnail" src={hero} alt={name} />
       </div>
       <div className="absolute bottom-0 left-0 bg-gradient-to-tr from-white to-transparent pr-2 pt-2">
         <h1>{name}</h1>
